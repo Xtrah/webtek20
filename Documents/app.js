5ecf1d23019cc9bcf2825e9341d3d388e8984ae3
Vemund Hassel <16759109+Vemhas@users.noreply.github.com>
2019-10-06 12:50:43 +0200

    add navBurger features.

---
+++ Documents/app.js
@@ -0,0 +1,30 @@
+//animation for the responsive navBurger.
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //Toggle navBurger
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        //Animate Links
+        navLinks.forEach((link,index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });
+    //Clicking navLinks li elements, reset all states.
+    nav.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+        navLinks.forEach((link) => {
+            link.style.animation = ''
+        });
+    });
+}
+//Invoking all the functions above in this statment.
+navSlide();
