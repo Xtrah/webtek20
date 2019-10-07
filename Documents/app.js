3b223012af67d720abb639c80cbe9210d2c2adf1
David <david.lekve@gmail.com>
2019-10-07 16:13:59 +0200

    Added a list and table in plan section

--- Documents/app.js
+++ Documents/app.js
@@ -11,7 +11,7 @@ const navSlide = () => {
             if (link.style.animation) {
                 link.style.animation = ''
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`
+                link.style.animation = `navLinkFade 0.25s ease forwards ${index / 14}s`
             }
         });
         //Burger Animation
