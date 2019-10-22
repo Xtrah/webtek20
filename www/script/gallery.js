e5f761302b74c88aca7e7351ecd4eb64da9f47d9
Vemund Hassel <16759109+Vemhas@users.noreply.github.com>
2019-10-22 16:50:56 +0200

    Carousel framework updates and changes.

--- www/script/gallery.js
+++ www/script/gallery.js
@@ -1,3 +1,4 @@
+//Desktop size image gallery
 const current = document.querySelector('#current')
 const imgs = document.querySelectorAll('.imgs img')
 const opacity = 0.6
@@ -24,3 +25,43 @@ function imgClick(e) {
     //Change oppacity to var
     e.target.style.opacity = opacity
 }
+
+// Responsiv slider carousel functionality
+const carouselSlide = document.querySelector('.carousel-slide')
+const carouselImages = document.querySelectorAll('.carousel-slide > img')
+// Buttons
+const prevBtn = document.querySelector('#prevBtn')
+const nextBtn = document.querySelector('#nextBtn')
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+
+//Button listeners
+nextBtn.addEventListener('click', ()=>{
+    if (counter >= carouselImages.length-1) return
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"
+    counter++
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+})
+prevBtn.addEventListener('click', ()=>{
+    if (counter <= 0) return
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"
+    counter--
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+})
+
+carouselSlide.addEventListener('transitionend', ()=>{
+    if (carouselImages[counter].id === 'lastClone') {
+        carouselSlide.style.transition = "none"
+        counter = carouselImages.length -2
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+    }
+    if (carouselImages[counter].id === 'firstClone') {
+        carouselSlide.style.transition = "none"
+        counter = carouselImages.length - counter
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
+    }
+})
