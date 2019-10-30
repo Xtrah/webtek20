cb7372ae8e4346950fd1024810baf8a7662ed09e
Vemund Hassel <16759109+Vemhas@users.noreply.github.com>
2019-10-30 16:44:09 +0100

    Change imgGallery, so same style/design mobile/Desktop

--- www/script/gallery.js
+++ www/script/gallery.js
@@ -1,31 +1,3 @@
-//Desktop size image gallery
-const current = document.querySelector('#current')
-const imgs = document.querySelectorAll('.imgs img')
-const opacity = 0.6
-
-// set first image opacity
-imgs[0].style.opacity = opacity
-
-imgs.forEach( img => 
-    img.addEventListener('click', imgClick))
-
-function imgClick(e) {
-    // reset opacity on all img
-    imgs.forEach(img => (img.style.opacity = 1))
-
-    // Change current image of source of clicked image
-    current.src = e.target.src
-
-    // Add fade-in class
-    current.classList.add('fade-in')
-
-    //Remove fade-in class after .5 seconds
-    setTimeout(() => current.classList.remove('fade-in'), 500)
-
-    //Change oppacity to var
-    e.target.style.opacity = opacity
-}
-
 // Responsiv slider carousel functionality
 const carouselSlide = document.querySelector('.carousel-slide')
 const carouselImages = document.querySelectorAll('.carousel-slide > img')
