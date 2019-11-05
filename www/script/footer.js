2b8653627400a230c1468a33670080f7f1c3dcc4
Vemund Hassel <16759109+Vemhas@users.noreply.github.com>
2019-11-05 15:58:41 +0100

    FIx icon and styling for the footer.

--- www/script/footer.js
+++ www/script/footer.js
@@ -1,20 +1,32 @@
 var footer = document.createElement("section")
 var footerOne = document.createElement("div")
 var footerTwo = document.createElement("div")
-var footerOneContent = document.createTextNode("Vigbjørn Hassel : +47 987 65 432")
-var footerTwoContent = document.createTextNode("©2019 Hassel Software AS")
+var pOne = document.createElement("h4")
+var pTwo = document.createElement("h4")
+var pOneText = document.createTextNode("Vigbjørn Hassel : +47 987 65 432")
+var pTwoText = document.createTextNode("©2019 Hassel Software AS")
 
+// var footerOneContent = document.createTextNode("Vigbjørn Hassel : +47 987 65 432")
+// var footerTwoContent = document.createTextNode("©2019 Hassel Software AS")
+var img = document.createElement("img")
+
+img.src = "img/contactIcon.png"
+img.alt = "footerPhoneIcon"
+img.id = "footerImg"
 
 footer.appendChild(footerOne)
+pOne.appendChild(pOneText)
 footer.appendChild(footerTwo)
-footerOne.appendChild(footerOneContent)
-footerTwo.appendChild(footerTwoContent)
+pTwo.appendChild(pTwoText)
+footerOne.appendChild(img)
+footerOne.appendChild(pOne)
+footerTwo.appendChild(pTwo)
 footer.setAttribute("id", "footer")
 footerOne.setAttribute("id", "footerOne")
 footerTwo.setAttribute("id", "footerTwo")
+pOne.setAttribute("id", "footerContactInfo")
+pTwo.setAttribute("id", "footerCopyRight")
 
 document.querySelector("body").appendChild(footer)
 console.log("hei big boy")
-
-
 
