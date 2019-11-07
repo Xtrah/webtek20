571b195747e314d1c3d15033c2fae238464eb7b2
Vemund Hassel <16759109+Vemhas@users.noreply.github.com>
2019-11-07 20:03:32 +0100

    modulizing navbruger after load

--- www/script/loadingScreen.js
+++ www/script/loadingScreen.js
@@ -1,7 +1,11 @@
-import {gallery} from './gallery.js';
+import {gallery} from './gallery.js'
+import {navSlide} from './navBurger.js'
+
 window.addEventListener("load", () => {
 	const loadingScreen = document.getElementById("loadingScreen")
 	loadingScreen.setAttribute("class", "loadingFinished")
 	// Calling gallery functionality after page load
+	navSlide()
 	gallery()
+
 }) 
