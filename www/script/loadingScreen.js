f0f814c8d876fb9f27884042caac922816c6c67d
Vemund Hassel <16759109+Vemhas@users.noreply.github.com>
2019-11-07 18:58:24 +0100

    import fallery function and call gallery

--- www/script/loadingScreen.js
+++ www/script/loadingScreen.js
@@ -2,7 +2,6 @@ import {gallery} from './gallery.js';
 window.addEventListener("load", () => {
 	const loadingScreen = document.getElementById("loadingScreen")
 	loadingScreen.setAttribute("class", "loadingFinished")
-	console.log("Is loaded")
+	// Calling gallery functionality after page load
 	gallery()
-	console.log("Nå har gallery funk kjørt fra loading.js")
 }) 
