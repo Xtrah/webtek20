9f1365283beb569a052e3afea938dd59df49f05b
Livia <lestokke@ntnu.stud.no>
2019-11-07 17:45:01 +0100

    Removed console log and added comments

--- www/script/loadingScreen.js
+++ www/script/loadingScreen.js
@@ -1,5 +1,6 @@
+/* Function calls when window is loaded */
 window.addEventListener("load", () => {
 	const loadingScreen = document.getElementById("loadingScreen")
+	/* Applies the loadingFinished class to the loading screen so it dissapears*/
 	loadingScreen.setAttribute("class", "loadingFinished")
-	console.log("Is loaded")
 }) 
