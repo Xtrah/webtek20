e5dc2d727f4f65f0712f23c31d83aac793bfd47b
Vemund Hassel <16759109+Vemhas@users.noreply.github.com>
2019-11-08 18:06:07 +0100

    Added comments to javascript + P3 changes gallery

--- www/script/gallery.js
+++ www/script/gallery.js
@@ -9,30 +9,35 @@ export function gallery(){
     const prevBtn = document.querySelector('#prevBtn')
     const nextBtn = document.querySelector('#nextBtn')
 
-    //Counter
+    //Counter, indicationg where we are on the horizintal image "scale"
     let counter = 1;
     let size = carouselImages[0].clientWidth
 
+    //Start on the first picture, not the "last clone"
     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
 
-    //Button listeners
+    // Button listeners
     // Next/previous image functionality, all images are stacked next to each other horoziontally, 
     // and then next image is displayed by translate/transformX with the current images width times the counter that indicates where in the cycle we are.
 
     nextBtn.addEventListener('click', ()=>{
+        //If statement to handle fast clicking of next/prev button
         if (counter >= carouselImages.length-1) return
         carouselSlide.style.transition = "transform 0.4s ease-in-out"
         counter++
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
     })
     prevBtn.addEventListener('click', ()=>{
+        //If statement to handle fast clicking of next/prev button
         if (counter <= 0) return
         carouselSlide.style.transition = "transform 0.4s ease-in-out"
         counter--
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
     })
 
-    //Adding first and last clone, to create an infinate loop, so that we can go either way "full circle".
+    // Adding first and last clone, to create an infinate loop, so that we can go either way "full circle".
+    // Resetting the transition style added above, and jumping to first/last clone without the transform style. 
+
     carouselSlide.addEventListener('transitionend', ()=>{
         if (carouselImages[counter].id === 'lastClone') {
             carouselSlide.style.transition = "none"
@@ -48,4 +53,6 @@ export function gallery(){
 }
 //Calling the gallery function every time the browsers viewport resizes to handle issues with horisontal images, and next one it transition either way X image width.
 window.addEventListener('resize', gallery)
-// Inspiration for this image gallery is partly from W3Schools, MDN, and  DevEd from youtube.com
+// Inspiration for this image gallery is partly from W3Schools,general use of MDN, and  DevEd from youtube.com
+// https://www.w3schools.com/howto/howto_js_slideshow.asp
+// https://www.youtube.com/watch?v=KcdBOoK3Pfw
