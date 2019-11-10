98d064f7c9f0e0cadf97e05d5189bc12d12d3693
Livia <lestokke@ntnu.stud.no>
2019-11-10 16:20:09 +0100

    Added comments to contactUs.js

--- www/script/contactUs.js
+++ www/script/contactUs.js
@@ -1,10 +1,13 @@
+// Getting the submit button and adding click listener
 let submitButton = document.getElementById("submitButton")
 submitButton.addEventListener("click", submitForm)
+// Getting all form input elements to later check for valid input
 let form = document.getElementById("contactUsForm")
 let name = document.getElementById("name")
 let email = document.getElementById("email")
 let telephone = document.getElementById("telephone")
 function submitForm(event){
+	// Checking for validity. If valid clear form and alert user that the form has been "sent"
 	if (name.checkValidity() && email.checkValidity() && telephone.checkValidity()){
 		event.preventDefault()
 		form.reset()
